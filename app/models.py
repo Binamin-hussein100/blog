28046b900ca8af16cc0f17d97af32c93491dfe50
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-08-25 06:33:13 +0300

    attempted 1st migration

--- app/models.py
+++ app/models.py
@@ -16,7 +16,7 @@ class Quote:
         self.quote = quote
 
 
-class User(UserMixin,db.Model):
+class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
