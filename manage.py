32204e89239fbda268a87384134c805da1d0dff9
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-08-25 08:35:19 +0300

    deployment

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate
-from app.models import User
+from app.models import User,Subscriber,Comment,Blog
 
 # creating an app instance
-app = create_app('production')
+app = create_app('development')
 
 
 
@@ -17,5 +17,5 @@ manager.add_command('db')
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app = app, db = db , User = User)
+    return dict(app = app, db = db , User = User,Subscriber = Subscriber, Comment = Comment,Blog = Blog)
 
